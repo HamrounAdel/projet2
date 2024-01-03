8e21ca194dc55d0c16592f50393d3784fe428cbc
HamrounAdel <HamrounAdel@email.fr>
2024-01-03 23:14:27 +0100

    ta7ssine fil carte

--- front/src/App.js
+++ front/src/App.js
@@ -6,8 +6,6 @@ import {Routes,Route} from 'react-router-dom'
 import RouteUser from './composant/privateRoute/RouteUser';
 import AddRdv from './composant/Rdv/AddRdv'; 
 import DoctorList from './composant/doctor/js/DoctorList';
-
-import ProfilDoctor from './composant/doctor/js/ProfilDoctor';
 import RegisterUser from './composant/patient/js/RegisterUser';
 import LoginUser from './composant/patient/js/LoginUser';
 import LoginDoctor from './composant/doctor/js/LoginDoctor';
@@ -26,25 +24,27 @@ function App() {
       
    <Routes>
      {/* <Route path='/te' element={<Profil/>}/>  */}
+     <Route path='/' element={<Home/>}/>
+
     <Route path='/updatRdv/:id' element={<UpdateRdv/>}/>
    <Route path='/rdv/consulter' element={<ListResPat/>}/>
    <Route path='/consulter/rdvDoc' element={<ListRdvDoc/>}/>
-   <Route path='/' element={<Home/>}/>
+   <Route path='/doctor/listrdv' element={<ListRDV/>}/>
+    <Route path='/patient/rdv'   element={<AddRdv/>}/>
+
    <Route path='/auth' element={<RouteUser/>}/>
    <Route  path='/login/User'  element={<LoginUser/>}/>
    <Route path='/register/User' element={<RegisterUser/>}/>
-    <Route path='//register/Doctor' element={<RegisterDoctor/>}/>
-    <Route path='/login/Doctor' element={<LoginDoctor/>}/>
-    {/* <Route path='/admin' element={<Admin/>}/> */}
-  
+   <Route path='/patient/monprofile' element={<Profil/>}/>
+   
      <Route path='/doct' element={<RouteDoctor/>}/>  
-     <Route path='/doctor/listrdv' element={<ListRDV/>}/>
-     <Route path='/doctor/updatRdv' element={<UpdateRdv/>}/>
-    <Route path='/patient/rdv'   element={<AddRdv/>}/>
-    <Route path='/patient/monprofile' element={<Profil/>}/>
+     <Route path='//register/Doctor' element={<RegisterDoctor/>}/>
+    <Route path='/login/Doctor' element={<LoginDoctor/>}/>
     <Route path='/doctor/monprofile' element={<DoctProfil/>}/>
     <Route path='/patient/medecines'  element={<DoctorList/>}/>
     
+
+    {/* <Route path='/doctor/updatRdv' element={<UpdateRdv/>}/> */}
    </Routes>
     </div>
   );
